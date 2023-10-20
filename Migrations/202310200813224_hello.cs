6b5c117e1641b1278b47fd7b3a406a769c8e6f31
Crisiroid <csiroid@gmail.com>
2023-10-20 11:45:55 +0330

    Update: Databse Error fixed

---
+++ Migrations/202310200813224_hello.cs
@@ -0,0 +1,42 @@
+﻿namespace EventFlow.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class hello : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Email = c.String(nullable: false),
+                        Password = c.String(nullable: false),
+                        PhoneNumber = c.String(nullable: false),
+                        Role = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            DropTable("dbo.Admins");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.Admins",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Username = c.String(nullable: false),
+                        Password = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            DropTable("dbo.Customers");
+        }
+    }
+}
---
+++ Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace EventFlow.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EventFlow.Data.EventFlowDB>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "EventFlow.Data.EventFlowDB";
+        }
+
+        protected override void Seed(EventFlow.Data.EventFlowDB context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
